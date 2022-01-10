f672d02c4fc8ccff0bbf6ec1ae985626e7a798f7
Pierre Jordi Guettaf <pierrejordiguettaf@gmail.com>
2022-01-10 13:44:51 +0100

    maj scorer et pipeline

--- soccer_predict/pipeline.py
+++ soccer_predict/pipeline.py
@@ -5,7 +5,7 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.metrics import make_scorer
-from sklearn.base import ClassifierMixin, BaseEstimator, TransformerMixin
+from sklearn.base import ClassifierMixin, BaseEstimator
 from sklearn import set_config; set_config(display='diagram')
 
 def data_split(data, list_y, list_X, classification):
@@ -43,11 +43,10 @@ def cross_validation_score(pipe, data, list_y, list_X, cv=5, classification=Fals
 
     """
     X_train, X_test, y_train, y_test = data_split(data, list_y, list_X, classification)
-    #import ipdb; ipdb.set_trace()
     if classification:
         cv_score = cross_val_score(pipe, X_train, y_train, cv=cv,scoring='accuracy')
     else:
-        cv_score = cross_val_score(pipe, X_train, y_train, cv=cv,scoring='accuracy')
+        cv_score = cross_val_score(pipe, X_train, y_train, cv=cv,scoring=make_scorer(custom_score))
     
     return {'cv_score':cv_score, 'X_train':X_train, 'X_test':X_test, 'y_train':y_train, 'y_test':y_test}
 
@@ -58,52 +57,47 @@ class Posttreat(BaseEstimator, ClassifierMixin):
     def fit(self,X, y):
         return self
     def predict(self, X, y=None):
-        import ipdb; ipdb.set_trace()
         # Ajouter la logique de post-traitement ici
-        y_pred = pd.DataFrame(y_p)
-        y_test = pd.DataFrame(y_t)
+        y_pred = pd.DataFrame(X)
         # création des colonnes de résultat
         y_pred['res'] = y_pred[0]-y_pred[1]
-        y_test['res'] = y_test['homeGoals'] - y_test['awayGoals']
         # conversion des résultats pour calcul du score
-        y_pred['res'] = y_pred['res'].apply(lambda x: 3 if x > self.indice_tole else 0 if x < -self.indice_tole else 1)
-        y_test['res'] = y_test['res'].apply(lambda x: 3 if x > self.indice_tole else 0 if x < -self.indice_tole else 1)
-
-        return (y_pred['res'] - y_test['res'].reset_index(drop=True))
+        y_pred_new = y_pred['res'].apply(lambda x: 3 if x > self.indice_tole else 0 if x < -self.indice_tole else 1)
+        
+        return (y_pred_new.reset_index(drop=True))
     
-# def custom_score(y_t, y_p, indice_tol=0.1):
-#     """
-#     Paramètres obligatoires:
+def custom_score(y_t, y_p, indice_tol=0.1):
+    """
+    Paramètres obligatoires:
 
-#         y_t: np.array du y_test (np.ndarray)
-#         y_p: np.array du y_pred (np.ndarray)
+        y_t: np.array du y_test (np.ndarray)
+        y_p: np.array du y_pred (np.ndarray)
 
-#     Paramètres optionnel:
+    Paramètres optionnel:
 
-#         indice_tol: Indice de tolérance: différence déterminant l'intervalle comprenant les matchs nuls dans la prédiction.(float)
-#                     Exemple: indice_tol=0.1 -> Tout les matchs ayant une différence de buts entre [0.1|-0.1] sont un résultat nul 
-#     """
+        indice_tol: Indice de tolérance: différence déterminant l'intervalle comprenant les matchs nuls dans la prédiction.(float)
+                    Exemple: indice_tol=0.1 -> Tout les matchs ayant une différence de buts entre [0.1|-0.1] sont un résultat nul 
+    """
     
-#     # réassignation des argument en pd.DataFrame
-#     y_pred = pd.DataFrame(y_p)
-#     y_test = pd.DataFrame(y_t)
-#     # création des colonnes de résultat
-#     y_pred['res'] = y_pred[0]-y_pred[1]
-#     y_test['res'] = y_test['homeGoals'] - y_test['awayGoals']
-#     # conversion des résultats pour calcul du score
-#     y_pred['res'] = y_pred['res'].apply(lambda x: 3 if x > indice_tol else 0 if x < -indice_tol else 1)
-#     y_test['res'] = y_test['res'].apply(lambda x: 3 if x > indice_tol else 0 if x < -indice_tol else 1)
-#     # calcul de l'accuracy
-#     accuracy = (y_pred['res'] - y_test['res'].reset_index(drop=True)).value_counts()[0]\
-#     /(y_pred['res'] - y_test['res'].reset_index(drop=True)).value_counts().sum()
+    # réassignation des argument en pd.DataFrame
+    y_pred = pd.DataFrame(y_p)
+    y_test = pd.DataFrame(y_t)
+    # création des colonnes de résultat
+    y_test['res'] = y_test['homeGoals'] - y_test['awayGoals']
+    # conversion des résultats pour calcul du score
+    y_test['res'] = y_test['res'].apply(lambda x: 3 if x > indice_tol else 0 if x < -indice_tol else 1)
+    y_test_new = pd.DataFrame(y_test['res'].reset_index(drop=True))
+    # calcul de l'accuracy
+    accuracy = (y_pred - y_test_new).value_counts()[0]\
+    /(y_pred - y_test_new).value_counts().sum()
     
-#     return accuracy
+    return accuracy
 
-def RegressorWrapper(Regressor):
-    class WrappedRegressor(Regressor, TransformerMixin):
+class CustomMultiouputwrapper(MultiOutputRegressor):
         def transform(self, X, y=None):
             return self.predict(X)
-    return WrappedRegressor
+        def fit_tranform(self, X, y):
+            return self.fit(X, y).predict(X)
     
 
 def pipeline(scaler='StandardScaler',
@@ -132,7 +126,7 @@ def pipeline(scaler='StandardScaler',
     if classification:
         return make_pipeline(sc(), md())
     else:
-        return make_pipeline(sc(), RegressorWrapper(md)(),Posttreat())
+        return make_pipeline(sc(), CustomMultiouputwrapper(md()),Posttreat())
     
 
 def get_train_pipeline(data, list_y, list_X, cv=5,
@@ -158,13 +152,12 @@ def get_train_pipeline(data, list_y, list_X, cv=5,
     # creation du pipeline
     pipe = pipeline(scaler, model_path, model_name, classification)
     # cross validation du pipeline
-    temp_res = cross_validation_score(pipe, data, list_y, list_X, cv, classification)
+    temp_res = cross_validation_score(pipe, data, list_y, list_X,classification, cv=cv)
     # récupération du score et du dataset splitté
     cv_score = temp_res['cv_score']; X_train = temp_res['X_train']; X_test = temp_res['X_test']
     y_train = temp_res['y_train']; y_test = temp_res['y_test']
     
     # fit du pipeline
-    #import ipdb; ipdb.set_trace()
     pipe.fit(X_train, y_train)
     
     if return_full_set:
@@ -226,7 +219,7 @@ def grid_search(pipe, X_train, y_train,cv=5):
     grid_search = GridSearchCV(pipe,
                                param_grid=grid_search_params,
                                cv=cv,
-                               scoring=custom_scorer,
+                               scoring=make_scorer(custom_score),
                                n_jobs = -1
                                )
 
@@ -237,12 +230,3 @@ def grid_search(pipe, X_train, y_train,cv=5):
     print(grid_search.best_score_)
     
     return grid_search
-
-
-
-
-
-
-
-
-
